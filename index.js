c2bb2b039f4121d465b7c0cbe1b48e7596caa10f
Noah <36684490+SoggyBacon3@users.noreply.github.com>
2018-05-23 20:24:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+'use strict';
+
+// Enter your js code Here
+var myPickle;
+var Abdul;
+var Jesus;
+var Pray;
+
+
+ myPickle = prompt ( "Your Uncle Abdul is coming");
+
+if(myPickle == "Greet Abdul" )
+{
+  alert("SCREEEEEEEEECH");
+}
+else if (myPickle == "eat my fat dick") {
+alert("Gladly")
+}
+
+else if (myPickle == "Go away") {
+  alert("Abdul is coming")
+}
+else if (myPickle == "run") {
+  alert("you can't run")
+}
+
+
+else {
+    alert("You can't escape");
+}
+
+Abdul = prompt("He is getting closer");
+
+if(Abdul == "grab gun"){
+  alert("Abdul ate your gun when you were three years old")
+}
+
+else if (Abdul == "hide") {
+  alert("Abdul sees all")
+}
+
+else if ("help") {
+  alert("No one can help you now")
+}
+
+else {
+  alert("Coming closer")
+}
+
+
+Jesus = prompt("Abdul is behind you, what do you do?" +
+  "Pray?  Beg for mercy?  Seduce?")
+
+if (Jesus == "Seduce") {
+  alert("Abdul is tempted by the child... yes... you will do nicely... come with Abdul.  Abdul takes you to his house where you become his personel slave.  ENDING?")
+}
+else if (Jesus == "Pray") {
+  Pray = prompt("who do you pray to?") {
+    if (Pray == "Gus"){
+      alert("You pray to Gus for protection.  Nothing happens as Abdul comes closer.  Suddenly, Gus floats down from the heavens and stares at Abdul.  Abdul explodes into a small pile of flowers.  GOOD ENDING?")
+    }
+
+    else if (Pray == "Weird Lime") {
+      alert("You bow and start to worship Weird Lime.  Suddenly, you feel a shift in the Force, a great power has reawoken to come to your aid against this foul creature.  Abdul disapears and in his place is a small tupperware with an angry inchworm inside, you can tell that this is Abdul.  GOOD ENDING?")
+    }
+    else {
+      alert("You pray to God for salvation.  Nothing happens.  Abdul ends your life and takes your soul for his own.  WORST ENDING?")
+    }
+  }
+
+  }
+} else if (Jesus == "Beg for mercy") {
+  alert("Abdul ignores your cries for mercy and he rips your throat with his teeth.  BAD ENDING?")
+} else {
+  alert("Abdul kills you.  LAZY ENDING?")
+}
